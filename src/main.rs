a439620601a87036863603cee874b63c3d66dcd6
othiagos <thg.santos@outlook.com>
2024-06-15 15:11:16 -0300

    Create the input parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod sudoku_parse;
+
+use std::{fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut file_handle = File::open("input/input1.txt").expect("Can not open the file");
+
+    let mut file_content = String::new();
+    file_handle.read_to_string(&mut file_content).expect("Can not be read the content of the file");
+
+    let mut sudoku_lines: Vec<&str> = file_content.split_terminator(" ").collect();
+    let sudoku_line_option = sudoku_parse::parse(sudoku_lines);
+    sudoku_lines = sudoku_line_option.expect("Informed Sudoku is not valid");
+
+    println!("{:?}", sudoku_lines);
+    
 }
